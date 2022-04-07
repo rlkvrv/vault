c353df000cd6f86647ae31c03300136812a978a9
Roman Yarlykov <roman.yarlykov@metalamp.io>
2022-04-07 11:10:46 +0700

    refactor withdraw and redeem functions
    - also updated tests

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -33,26 +33,82 @@ contract Vault is ERC20 {
         uint256 shares
     );
 
+    function maxDeposit(address) external pure returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function deposit(uint256 assets, address receiver)
+        external
+        returns (uint256 shares)
+    {
+        require((shares = previewDeposit(assets)) != 0, "Vault: ZERO_SHARES");
+
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+    }
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) external returns (uint256 shares) {
+        shares = convertToShares(assets);
+        if (msg.sender != owner) {
+            _spendAllowance(owner, receiver, assets);
+        }
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+    }
+
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) external returns (uint256 assets) {
+        require((assets = convertToAssets(shares)) != 0, "Vault: ZERO_ASSETS");
+
+        if (msg.sender != owner) {
+            _spendAllowance(owner, receiver, assets);
+        }
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        asset.safeTransfer(receiver, assets);
+    }
+
     function totalAssets() public view returns (uint256 totalManagedAssets) {
         return asset.balanceOf(address(this)) + totalDebt;
     }
 
-    function convertToShares(uint256 assets) public view returns (uint256) {
+    function convertToShares(uint256 assets)
+        public
+        view
+        returns (uint256 shares)
+    {
         uint256 totalSupply = totalSupply();
 
         return
             totalSupply == 0 ? assets : (assets * totalSupply) / totalAssets();
     }
 
-    function convertToAssets(uint256 shares) public view returns (uint256) {
+    function convertToAssets(uint256 shares)
+        public
+        view
+        returns (uint256 assets)
+    {
         uint256 totalSupply = totalSupply();
 
         return
             totalSupply == 0 ? shares : (shares * totalAssets()) / totalSupply;
-    }
-
-    function maxDeposit(address) public pure returns (uint256) {
-        return type(uint256).max;
     }
 
     function previewDeposit(uint256 assets)
@@ -63,59 +119,11 @@ contract Vault is ERC20 {
         return convertToShares(assets);
     }
 
-    function deposit(uint256 assets, address receiver)
-        external
-        returns (uint256 shares)
-    {
-        require((shares = previewDeposit(assets)) != 0, "Vault: ZERO_SHARES");
-        asset.safeTransferFrom(msg.sender, address(this), assets);
-
-        _mint(receiver, shares);
-
-        emit Deposit(msg.sender, receiver, assets, shares);
-    }
-
-    function maxWithdraw(address owner) public view returns (uint256) {
+    function maxWithdraw(address owner) public view returns (uint256 assets) {
         return convertToAssets(balanceOf(owner));
     }
 
-    function withdraw(
-        uint256 assets,
-        address receiver,
-        address owner
-    ) public returns (uint256 shares) {
-        shares = convertToShares(assets);
-        _burn(owner, shares);
-
-        if (msg.sender != owner) {
-            asset.safeTransferFrom(owner, receiver, assets);
-        } else {
-            asset.safeTransfer(receiver, assets);
-        }
-        emit Withdraw(msg.sender, receiver, owner, assets, shares);
-        // beforeWithdraw(assets, shares);
-    }
-
-    function redeem(
-        uint256 shares,
-        address receiver,
-        address owner
-    ) public virtual returns (uint256 assets) {
-        if (msg.sender != owner) {
-            uint256 allowed = allowance(owner, msg.sender); // Saves gas for limited approvals.
-            if (allowed != type(uint256).max)
-                approve(msg.sender, allowed - shares);
-        }
-
-        // Check for rounding error since we round down in previewRedeem.
-        require((assets = convertToAssets(shares)) != 0, "ZERO_ASSETS");
-
-        // beforeWithdraw(assets, shares);
-
-        _burn(owner, shares);
-
-        emit Withdraw(msg.sender, receiver, owner, assets, shares);
-
-        asset.safeTransfer(receiver, assets);
+    function maxRedeem(address owner) public view returns (uint256 shares) {
+        return balanceOf(owner);
     }
 }
--- test/vault.js
+++ test/vault.js
@@ -4,6 +4,7 @@ const { ethers } = require("hardhat");
 describe("Vault", function () {
   let token;
   let vault;
+  let maxUint = ethers.BigNumber.from(2n ** 256n - 1n);
 
   beforeEach(async function () {
     [acc1, acc2] = await ethers.getSigners();
@@ -13,20 +14,46 @@ describe("Vault", function () {
 
     const Vault = await ethers.getContractFactory("Vault", acc1);
     vault = await (await Vault.deploy(token.address)).deployed();
+    await token.approve(vault.address, 10000);
   })
 
-  it("should be", async function () {
-    await token.approve(vault.address, 10000);
-    expect(await token.allowance(acc1.address, vault.address)).to.eq(10000);
+  it("maxDeposit should be return maxUint", async function () {
+    expect(await vault.maxDeposit(acc1.address)).eq(maxUint);
+  });
+
+  it("convertToShares should be return 100", async function () {
+    expect(await vault.convertToShares(100)).eq(100);
+  });
+
+  it("convertToAssets should be return 100", async function () {
+    expect(await vault.convertToAssets(100)).eq(100);
+  });
+
+  it("maxWithdraw should be return assets owner balanceOf", async function () {
     await vault.deposit(1000, acc1.address);
-    // await vault.approve(acc2.address, 100);
-    await vault.connect(acc2).withdraw(1000, acc2.address, acc1.address);
-    console.log('balance', await token.balanceOf(acc2.address));
-    // await vault.connect(acc2).withdraw(100, acc2.address, acc1.address);
-    // await vault.connect(acc2).withdraw(100, acc2.address, acc1.address);
-    // await vault.connect(acc2).withdraw(100, acc2.address, acc1.address);
-    // await vault.connect(acc2).withdraw(100, acc2.address, acc1.address);
-    console.log(await vault.allowance(acc1.address, acc2.address));
-    console.log(await vault.allowance(acc2.address, acc2.address));
+    expect(await vault.maxWithdraw(acc1.address)).eq(1000);
+  });
+
+  it("maxRedeem should be return shares owner balanceOf", async function () {
+    await vault.deposit(1000, acc1.address);
+    expect(await vault.maxRedeem(acc1.address)).eq(1000);
+  });
+
+  it("withdraw should be correct work", async function () {
+    await vault.deposit(1000, acc1.address);
+    await vault.approve(acc2.address, 100);
+    await vault.connect(acc2).withdraw(50, acc2.address, acc1.address);
+    expect(await token.balanceOf(acc2.address)).eq(50);
+    expect(await vault.allowance(acc1.address, acc2.address)).eq(50);
+    await expect(vault.connect(acc2).withdraw(100, acc2.address, acc1.address)).revertedWith('ERC20: insufficient allowance');
+  });
+
+  it("redeem should be correct work", async function () {
+    await vault.deposit(1000, acc1.address);
+    await vault.approve(acc2.address, 100);
+    await vault.connect(acc2).redeem(50, acc2.address, acc1.address);
+    expect(await token.balanceOf(acc2.address)).eq(50);
+    expect(await vault.allowance(acc1.address, acc2.address)).eq(50);
+    await expect(vault.connect(acc2).redeem(100, acc2.address, acc1.address)).revertedWith('ERC20: insufficient allowance');
   });
 });
